ca6d373853730da6a1b972136622fe5f7c038470
hjemei <hamid.jemei@brightfuture.co.uk>
2015-07-02 18:22:20 +0100

    initial commit

---
+++ src/main/java/com/jemei/courtbound/CourtBoundApplication.java
@@ -0,0 +1,12 @@
+package com.jemei.courtbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtBoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourtBoundApplication.class, args);
+    }
+}
